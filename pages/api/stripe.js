8cca3548434dc4e9c9f73f96ef47c2806e383e83
Aidan Williams <aidanw4@gmail.com>
2023-01-04 17:44:28 +0700

    update stripe API key

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,10 +1,6 @@
 import Stripe from 'stripe';
 
-// const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
-
-const stripe = new Stripe(
-  'sk_test_51MMQbWIc910o6dcJHMz4Fap0lLUZFY6bsFNFpaPXqbbNFxAS6Cflp7fvSB5vEx0ENyYal6tWfoo3Lydvcb8zGDvQ00r6Xy6wl8'
-);
+const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
